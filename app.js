757a16a3b2d7bea4749c4aa77f5d655b0a4037d5
Ramses Anceno <ramses.social0@gmail.com>
2025-08-06 22:58:06 -0600

    Implementar funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+
+function agregarAmigo() {
+	let inputNombre = document.getElementById('amigo');
+	let nombre = inputNombre.value
+
+	if (nombre === '') {
+		alert('Por favor, ingresa un nombre.');
+		return;
+	} else {
+		inputNombre.value = '';
+		inputNombre.focus();
+		amigos.push(nombre);
+	}
+}
