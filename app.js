605fd15dff7dae4ce4d241b158afa10155dccf6c
Ramses Anceno <ramses.social0@gmail.com>
2025-08-06 23:22:58 -0600

    Implementa una funcion para sortear a los amigos

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
+let inputNombre = document.getElementById('amigo');
 let amigos = [];
 
 
 function agregarAmigo() {
-	let inputNombre = document.getElementById('amigo');
-	let nombre = inputNombre.value
-
-	if (nombre === '') {
+	if (inputNombre.value === '')
 		alert('Por favor, ingresa un nombre.');
-		return;
-	} else {
+	else {
+		amigos.push(inputNombre.value);
+
 		inputNombre.value = '';
 		inputNombre.focus();
-		amigos.push(nombre);
+
 		actualizarLista();
 	}
 }
 
 function actualizarLista() {
-	let listaAmigos = document.getElementById('listaAmigos');
 	listaAmigos.innerHTML = '';
+	resultado.innerHTML = '';
 
 	for (let index = 0; index < amigos.length; index++)
 		listaAmigos.innerHTML += `<li>${amigos[index]}</li>`;
+}
+
+function sortearAmigo() {
+	listaAmigos.innerHTML = '';
+	resultado.innerHTML = '';
+
+	if (amigos.length === 0)
+		alert('No hay amigos para sortear. Por favor, agrega al menos un amigo.');
+	else {
+		let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+		resultado.innerHTML = `<li>El amigo sorteado es: ${amigos[indiceAleatorio]}</li>`;
+	}
 }
